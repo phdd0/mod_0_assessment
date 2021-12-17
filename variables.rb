e631e88c2ab9ca97f6ed4a9482d42b5857de1ef9
Paul J. Haddick <94933767+phdd0@users.noreply.github.com>
2021-12-17 13:35:21 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+movie = "The Matrix"
+is_entertaining = true
+movie = "Interstellar"
+num_4 = 5.4002
+num_6 = 4.5002
+product = num_4 * num_6
+favorite_foods = ["peas", "beans", "meats"]
+favorite_foods.shift
+puts "This is the number of characters in the variable called 'movie': " + "#{movie.length}"
+
