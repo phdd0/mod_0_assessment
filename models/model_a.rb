3cce4e084ab8938ea9b793e0a83b73f40bba9cf8
Paul J. Haddick <94933767+phdd0@users.noreply.github.com>
2021-12-17 13:38:56 -0700

    Add T-Rex hash model

---
+++ models/model_a.rb
@@ -0,0 +1,2 @@
+dino_tyrannasaurus_rex = { "nickname" => "T-Rex", "pose" => "Menacing", "height(m)" => 15, "weight(e)" => 8000, "is_menacing" => true, "eats_plants" => false, "central_incisor_white_tint" => [6, 9, 3], "egg_counts_per_brood" => [2, 3, 3, 1] }
+
